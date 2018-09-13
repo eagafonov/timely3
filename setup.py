35a096fd0ec6785d2d5d67d5850ec268e6f0c40e
Eugene Agafonov <e.a.agafonov@gmail.com>
2018-09-13 12:28:05 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='timely3',
+      version='0.0.0',
+      description='Stopwatch and performance measurment tools',
+      author='Eyugene Agafonov',
+      author_email='e.a.agafonov@gmail.com',
+      url='https://github.com/eagafonov/timely3',
+      packages=['timely3',
+                ],
+      license='',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: Python',
+                   'License :: OSI Approved :: MIT',
+                   ]
+      )
